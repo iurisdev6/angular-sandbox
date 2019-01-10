7576431571f76401c6ecdc82f1ee8dbf2c3ac6ae
Iuri_Strelet <iuri_strelet@Iuri-Strelets-MacBook-Pro.local>
2019-01-10 11:45:14 -0500

    feat: add products module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './modules/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,4 +1,4 @@
 export const VERSION = {
-  appVersion: '0.0.0',
-  buildDate: ''
+  appVersion: '0.0.2',
+  buildDate: '1-10-2019 11:40:29'
 };
